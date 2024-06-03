efb18aa538e93bebf64ddb62d3cc8825f084701d
Sameh Khasawneh <sameh.khasawneh@gmail.com>
2024-06-03 15:51:59 +0300

    fix docker-compose

--- dev_setup/up.sh
+++ dev_setup/up.sh
@@ -2,11 +2,11 @@
 
 SCRIPTPATH="$(pwd)/dev_setup"
 
-docker compose -f $SCRIPTPATH/docker-compose.yml up -d database
+docker-compose -f $SCRIPTPATH/docker-compose.yml up -d database
 
 sleep 3
 
 # SETUP DATABASE ROLES AND PERMISSIONS
 docker exec database-node sh -c "/cockroach/cockroach sql -u root --insecure  --host=database-node < /docker-entrypoint-initdb.d/init.sql"
 
-docker compose -f $SCRIPTPATH/docker-compose.yml up -d
+docker-compose -f $SCRIPTPATH/docker-compose.yml up -d
